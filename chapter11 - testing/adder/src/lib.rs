71176d9cf264eb10ecada0d3c02da6ffcf56b749
Yan Pitangui <yanpitangui@gmail.com>
2022-08-21 11:58:27 -0300

    Chapter 11.1: How to write tests

---
+++ chapter11 - testing/adder/src/lib.rs
@@ -0,0 +1,103 @@
+extern crate core;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+pub struct Guess {
+    value: i32
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value <1 {
+            panic!("Guess value must be greater than or equal to 0")
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100")
+        }
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    // #[test]
+    // fn failing_test() {
+    //     panic!("Failing test");
+    // }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1
+        };
+
+        assert!(!smaller.can_hold(&larger));
+
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(result.contains("Carol"));
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn is_this_correct() -> Result<(), String> {
+        if 2 + 3 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+}
