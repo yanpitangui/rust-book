1fdcad603cbd835a85fc9a821046ecbf3699eb42
Yan Pitangui <yanpitangui@gmail.com>
2022-08-24 20:29:59 -0300

    Chapter 13.2: Iterators Chapter 13.3: Refactoring minigrep to use iterators

---
+++ chapter13 - closures and iterators/minigrep/src/main.rs
@@ -0,0 +1,24 @@
+
+use std::{env, process};
+use std::error::Error;
+use minigrep::Config;
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+
+    Ok(())
+
+
+}
