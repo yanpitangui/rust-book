2cdbe3454855660f24f86c83f913914da2e1cc5f
Yan Pitangui <yanpitangui@gmail.com>
2022-08-24 21:29:14 -0300

    Chapter 14: cargo and crates.io

---
+++ chapter14 - more about cargo and crates/art/src/main.rs
@@ -0,0 +1,8 @@
+use yan_art_book::mix;
+use yan_art_book::PrimaryColor;
+
+fn main() {
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+    mix(red, yellow);
+}
---
+++ chapter14 - more about cargo and crates/my_crate/src/lib.rs
@@ -0,0 +1,23 @@
+//! # My crate
+//!
+//! `my_crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
+/// Adds one to the number given.
+/// # Arguments
+///
+/// * `x`: the number to be incremented
+///
+/// returns: i32
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
